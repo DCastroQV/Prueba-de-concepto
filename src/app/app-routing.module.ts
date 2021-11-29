b7d4a4640d600707479ce17d2f616212dae49e7b
Diego Alejandro Castro López <dcastro@qvision.us>
2021-11-29 10:50:20 -0500

    se crean los componentes para los requerimeinteos 9 10 11 y 12

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HolaNundoComponent } from './hola-nundo/hola-nundo.component';
+import { Req009Component } from './req009/req009.component';
+import { Req010Component } from './req010/req010.component';
+import { Req011Component } from './req011/req011.component';
+import { Req012Component } from './req012/req012.component';
 
 const routes: Routes = [
   { path: 'holamundo', component: HolaNundoComponent },
+  { path: 'req009', component: Req009Component },
+  { path: 'req010', component: Req010Component },
+  { path: 'req011', component: Req011Component },
+  { path: 'req012', component: Req012Component },
 ];
 
 @NgModule({
---
+++ src/app/req010/req010.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-req010',
+  templateUrl: './req010.component.html',
+  styleUrls: ['./req010.component.css']
+})
+export class Req010Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
