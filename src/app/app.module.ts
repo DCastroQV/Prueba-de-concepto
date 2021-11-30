73dc08a4d41a22b09a269463af63930cee887f8d
DCastroQV <75998832+DCastroQV@users.noreply.github.com>
2021-11-30 12:56:48 -0500

    Revert "desarrollo e implementacion del Requerimeiento RQ02"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HolaNundoComponent } from './hola-nundo/hola-nundo.component';
-import { Req009Component } from './req009/req009.component';
-import { Req010Component } from './req010/req010.component';
-import { Req011Component } from './req011/req011.component';
-import { Req012Component } from './req012/req012.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HolaNundoComponent,
-    Req009Component,
-    Req010Component,
-    Req011Component,
-    Req012Component
+    HolaNundoComponent
   ],
   imports: [
     BrowserModule,
