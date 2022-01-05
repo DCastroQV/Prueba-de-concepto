3f8fc4f82a232dc2f6ab02e1ea220faa9cc1f994
eapalacios57 <eapalacios57@misena.edu.co>
2022-01-05 11:27:55 -0500

    Se crea nuevo componente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HolaNundoComponent } from './hola-nundo/hola-nundo.component';
+import { InfoDesalComponent } from './info-desal/info-desal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: 'infodesaltest', component: InfoDesalComponent
+   }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HolaNundoComponent } from './hola-nundo/hola-nundo.component';
+import { InfoDesalComponent } from './info-desal/info-desal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HolaNundoComponent
+    HolaNundoComponent,
+    InfoDesalComponent
   ],
   imports: [
     BrowserModule,
